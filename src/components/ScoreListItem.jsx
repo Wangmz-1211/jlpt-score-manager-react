c56d9935dfe7893f17ae1c40e90b4b9da6bc31b9
wangmz <w2639764449@hotmail.com>
2023-10-25 23:52:58 +0800

    ✨ edit vocabulary data
    todo: other parts

--- src/components/ScoreListItem.jsx
+++ src/components/ScoreListItem.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from 'react'
-import { Card, Statistic, Modal, Button } from 'antd'
-import { deleteScoreRecord } from '../api/score.ts'
+import { Card, Statistic, Modal, Button, Typography, InputNumber , message} from 'antd'
+import { deleteScoreRecord, updateScoreRecord } from '../api/score.ts'
 import './ScoreListItem.css'
 import { connect } from 'react-redux'
 import actions from '../store/actions'
 
+const { Title } = Typography
+const titleStyle = {
+	display: 'flex',
+	justifyContent: 'space-between',
+}
+const numberInputStyle = {
+	width: 65,
+	margin: '0 10px',
+}
+const recordCopy = (record) => {
+	return {
+		...record,
+		vocabulary: { ...record.vocabulary },
+		grammar: { ...record.grammar },
+		reading: { ...record.reading },
+		listening: { ...record.listening },
+	}
+}
 function ScoreListItem(props) {
 	let { record } = props
 	let [modalVisible, setModalVisible] = useState(false),
-		[modalLoading, setModalLoading] = useState(false)
+		[modalLoading, setModalLoading] = useState(false),
+		[formData, setFormData] = useState(recordCopy(record))
 	return (
-		// todo: click the card -> show the detail information, which could be edited
 		<React.Fragment>
 			<Card
 				bordered={false}
@@ -32,11 +50,26 @@ function ScoreListItem(props) {
 				onCancel={() => {
 					setModalVisible(false)
 				}}
-				onOk={() => {
+				onOk={async () => {
+					// todo: update the information
+					const newRecord = await updateScoreRecord(
+						record._id,
+						formData
+					)
+					if (newRecord) {
+						props.replaceScoreRecordById(
+							record._id,
+							newRecord,
+							props.scoreRecords
+						)
+					} else {
+						message.error('update failure')
+					}
 					setModalLoading(true)
 					setModalLoading(false)
 					setModalVisible(false)
 				}}
+				okText='Edit'
 				footer={(_, { OkBtn, CancelBtn }) => {
 					return (
 						<React.Fragment>
@@ -80,7 +113,85 @@ function ScoreListItem(props) {
 						</React.Fragment>
 					)
 				}}
-			></Modal>
+				width={600}
+			>
+				<Typography
+					style={{
+						padding: '0 20px',
+					}}
+				>
+					<Title level={5} style={titleStyle}>
+						<span>Vocabulary</span>
+						<span>{record.vocabulary_score}</span>
+					</Title>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.vocabulary.v1}
+						onChange={(v) => {
+							formData.vocabulary.v1 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.vocabulary.v2}
+						onChange={(v) => {
+							formData.vocabulary.v2 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.vocabulary.v3}
+						onChange={(v) => {
+							formData.vocabulary.v3 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={7}
+						defaultValue={record.vocabulary.v4}
+						onChange={(v) => {
+							formData.vocabulary.v4 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.vocabulary.v5}
+						onChange={(v) => {
+							formData.vocabulary.v5 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.vocabulary.v6}
+						onChange={(v) => {
+							formData.vocabulary.v6 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<Title level={5} style={titleStyle}>
+						<span>Grammar</span>
+						<span>{record.grammar_score}</span>
+					</Title>
+					<Title level={5} style={titleStyle}>
+						<span>Reading</span>
+						<span>{record.reading_score}</span>
+					</Title>
+					<Title level={5} style={titleStyle}>
+						<span>Listening</span>
+						<span>{record.listening_score}</span>
+					</Title>
+				</Typography>
+			</Modal>
 		</React.Fragment>
 	)
 }
--- src/store/actions/score.js
+++ src/store/actions/score.js
@@ -1,17 +1,25 @@
 const scoreAction = {
-    setScoreRecords( scoreRecords) {
-        return {
-            type: 'SCORE_SET',
-            payload: scoreRecords 
-        }
-    },
-    clearScoreRecords() {
-        return {
-            type: 'SCORE_CLEAR',
-            payload: []
-        }
-    }
-
+	setScoreRecords(scoreRecords) {
+		return {
+			type: 'SCORE_SET',
+			payload: scoreRecords,
+		}
+	},
+	clearScoreRecords() {
+		return {
+			type: 'SCORE_CLEAR',
+			payload: [],
+		}
+	},
+	replaceScoreRecordById(id, newRecord, oldRecords) {
+		const newRecords = oldRecords.map((oldRecord) => {
+			return oldRecord._id === newRecord._id ? newRecord : oldRecord
+		})
+		return {
+			type: 'SCORE_REPLACE',
+			payload: newRecords,
+		}
+	},
 }
 
 export default scoreAction
