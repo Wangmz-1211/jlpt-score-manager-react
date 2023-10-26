a21841f940e46e3b3cfe84edae447be56929ec28
wangmz <w2639764449@hotmail.com>
2023-10-26 22:56:23 +0800

    ✨ edit score score

--- src/components/ScoreListItem.jsx
+++ src/components/ScoreListItem.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react'
-import { Card, Statistic, Modal, Button, Typography, InputNumber , message} from 'antd'
+import {
+	Card,
+	Statistic,
+	Modal,
+	Button,
+	Typography,
+	InputNumber,
+	message,
+	Tooltip,
+} from 'antd'
 import { deleteScoreRecord, updateScoreRecord } from '../api/score.ts'
 import './ScoreListItem.css'
 import { connect } from 'react-redux'
@@ -50,8 +59,8 @@ function ScoreListItem(props) {
 				onCancel={() => {
 					setModalVisible(false)
 				}}
+				confirmLoading={modalLoading}
 				onOk={async () => {
-					// todo: update the information
 					const newRecord = await updateScoreRecord(
 						record._id,
 						formData
@@ -62,6 +71,7 @@ function ScoreListItem(props) {
 							newRecord,
 							props.scoreRecords
 						)
+						message.success('The score has updated.', 2)
 					} else {
 						message.error('update failure')
 					}
@@ -94,7 +104,7 @@ function ScoreListItem(props) {
 												const newScoreRecords =
 													props.scoreRecords.filter(
 														(r) =>
-															r._id != record._id
+															r._id !== record._id
 													)
 												props.setScoreRecords(
 													newScoreRecords
@@ -182,14 +192,150 @@ function ScoreListItem(props) {
 						<span>Grammar</span>
 						<span>{record.grammar_score}</span>
 					</Title>
+					<InputNumber
+						min={0}
+						max={12}
+						defaultValue={record.grammar.g7}
+						onChange={(v) => {
+							formData.grammar.g7 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.grammar.g8}
+						onChange={(v) => {
+							formData.grammar.g8 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.grammar.g9}
+						onChange={(v) => {
+							formData.grammar.g9 = v
+						}}
+						style={numberInputStyle}
+					/>
 					<Title level={5} style={titleStyle}>
 						<span>Reading</span>
 						<span>{record.reading_score}</span>
 					</Title>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.reading.r10}
+						onChange={(v) => {
+							formData.reading.r10 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<Tooltip
+						placement='top'
+						title='The first and second questions of three articles.'
+					>
+						<InputNumber
+							min={0}
+							max={6}
+							defaultValue={record.reading.r11_1}
+							onChange={(v) => {
+								formData.reading.r11_1 = v
+							}}
+							style={numberInputStyle}
+						/>
+					</Tooltip>
+					<Tooltip
+						placement='top'
+						title='The third question of three articles.'
+					>
+						<InputNumber
+							min={0}
+							max={3}
+							defaultValue={record.reading.r11_2}
+							onChange={(v) => {
+								formData.reading.r11_2 = v
+							}}
+							style={numberInputStyle}
+						/>
+					</Tooltip>
+					<InputNumber
+						min={0}
+						max={2}
+						defaultValue={record.reading.r12}
+						onChange={(v) => {
+							formData.reading.r12 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={3}
+						defaultValue={record.reading.r13}
+						onChange={(v) => {
+							formData.reading.r13 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={2}
+						defaultValue={record.reading.r14}
+						onChange={(v) => {
+							formData.reading.r14 = v
+						}}
+						style={numberInputStyle}
+					/>
 					<Title level={5} style={titleStyle}>
 						<span>Listening</span>
 						<span>{record.listening_score}</span>
 					</Title>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.listening.l1}
+						onChange={(v) => {
+							formData.listening.l1 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={6}
+						defaultValue={record.listening.l2}
+						onChange={(v) => {
+							formData.listening.l2 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={5}
+						defaultValue={record.listening.l3}
+						onChange={(v) => {
+							formData.listening.l3 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={12}
+						defaultValue={record.listening.l4}
+						onChange={(v) => {
+							formData.listening.l4 = v
+						}}
+						style={numberInputStyle}
+					/>
+					<InputNumber
+						min={0}
+						max={4}
+						defaultValue={record.listening.l5}
+						onChange={(v) => {
+							formData.listening.l5 = v
+						}}
+						style={numberInputStyle}
+					/>
 				</Typography>
 			</Modal>
 		</React.Fragment>
